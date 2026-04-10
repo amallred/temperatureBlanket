837ebf6b26a8a4c1215b6e03995ecf691f1f8101
Amanda Allred <amanda.barnett11@yahoo.com>
2026-04-09 20:12:41 -0400

    began js work

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 - Create an app that visualizes a temperature blanket for a chosen span of time with a chosen amount of custom colors
 - Connect to a weather API
 
-### Acknowledgements
+### Acknowledgements & References
 - CSS Reset from [Josh Comeau](https://www.joshwcomeau.com/css/custom-css-reset/)
+- [BEM structure](https://getbem.com/naming/)
+- [Javascript-friendly BEM](https://medium.com/trabe/a-more-javascript-friendly-bem-naming-convention-75c7f01ff736)
+  - Block: PascalCase
+  - Element: camelCase_oneUnderscore
+  - Modifier: camelCase___threeUnderscores
---
+++ index.js
@@ -0,0 +1,26 @@
+async function loadWeatherData (year, latitude, longitude, tempScale = "fahrenheit", timeZone = "2FNew_York") {
+try {
+    let res = await fetch(`https://open-meteo.com/en/docs/historical-weather-api?temperature_unit=${tempScale}&daily=temperature_2m_mean&timezone=America%${timeZone}&start_date=${year}-01-01&end_date=${year}-12-31&latitude=${latitude}&longitude=${longitude}`, {
+        method: GET
+    })
+
+} catch (error) {
+        console.error('error', error.message)
+    }
+}
+
+// async function collectData () {
+
+// }
+
+// Convert city name to coordinates
+// NEED TO FIND A WAY TO CONFIRM LOCATION (STATE/COUNTRY/ETC)
+async  function getCoordinates (cityName) {
+    try {
+        let res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&language=en&format=json`, {
+            method : GET
+        })
+    } catch (error) {
+        console.error('error', error.message)
+    }
+}
