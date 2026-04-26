910f459ee15a3d96243b8849dce96c94b26d2284
Amanda Allred <amanda.barnett11@yahoo.com>
2026-04-26 11:20:16 -0400

    working on node

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const form = document.querySelector('#DataForm')
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
 
-    const formData = new FormData(form) // need to look again at what this does/how it works
+//     const formData = new FormData(form) // need to look again at what this does/how it works
 
-    // const city = formData.get('city')
-    // const year = formData.get('year')
+//     // const city = formData.get('city')
+//     // const year = formData.get('year')
 
-    // const data = Object.fromEntries(formData.entries())
-    console.log(formData)
-})
+//     // const data = Object.fromEntries(formData.entries())
+//     console.log(formData)
+// })
+
+
+fetch('https://archive-api.open-meteo.com/v1/archive?latitude=35.222&longitude=-101.8313&start_date=2026-04-10&end_date=2026-04-24&hourly=temperature_2m&timezone=America%2FNew_York&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(e => console.error(e))
+
+// `https://open-meteo.com/en/docs/historical-weather-api?temperature_unit=${tempScale}&daily=temperature_2m_mean&timezone=America%${timeZone}&start_date=${year}-01-01&end_date=${year}-12-31&latitude=${latitude}&longitude=${longitude}`
 
 
 
 async function loadWeatherData (year, latitude, longitude, tempScale = "fahrenheit", timeZone = "2FNew_York") {
 try {
-    let res = await fetch(`https://open-meteo.com/en/docs/historical-weather-api?temperature_unit=${tempScale}&daily=temperature_2m_mean&timezone=America%${timeZone}&start_date=${year}-01-01&end_date=${year}-12-31&latitude=${latitude}&longitude=${longitude}`, {
+    let res = await fetch(`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${year}-01-01&end_date=${year}-12-31&hourly=temperature_2m&timezone=America%${timeZone}&temperature_unit=${tempScale}`, {
         method: GET
     })
 
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3500
+// const path = require('path')
+const PORT = 2026
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.static('./public'))
+
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+    console.log('Press Ctrl + C to end this process')
+})
