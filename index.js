54482f35c2c47c9ca34e4fbf17864e6057391229
Amanda Allred <amanda.barnett11@yahoo.com>
2026-04-26 10:47:29 -0400

    experiement with node and express

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const form = document.querySelector('#DataForm')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(form) // need to look again at what this does/how it works
+
+    // const city = formData.get('city')
+    // const year = formData.get('year')
+
+    // const data = Object.fromEntries(formData.entries())
+    console.log(formData)
+})
+
+
+
 async function loadWeatherData (year, latitude, longitude, tempScale = "fahrenheit", timeZone = "2FNew_York") {
 try {
     let res = await fetch(`https://open-meteo.com/en/docs/historical-weather-api?temperature_unit=${tempScale}&daily=temperature_2m_mean&timezone=America%${timeZone}&start_date=${year}-01-01&end_date=${year}-12-31&latitude=${latitude}&longitude=${longitude}`, {
@@ -14,8 +30,8 @@ try {
 // }
 
 // Convert city name to coordinates
-// NEED TO FIND A WAY TO CONFIRM LOCATION (STATE/COUNTRY/ETC)
-async  function getCoordinates (cityName) {
+    // NEED TO FIND A WAY TO CONFIRM LOCATION (STATE/COUNTRY/ETC)
+async function getCoordinates (cityName) {
     try {
         let res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&language=en&format=json`, {
             method : GET
