bf8c575a8982346ef0bb5b1574b699b8113d266e
Amanda Allred <amanda.barnett11@yahoo.com>
2026-04-26 11:31:14 -0400

    updated file structure, node working

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs/promises'
 
 const app = express()
 // const path = require('path')
 const PORT = 2026
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, './public')))
 
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
     console.log('Press Ctrl + C to end this process')
 })
