cede3826038806592f448ef04fbfcc8975b7ed93
Amanda Allred <amanda.barnett11@yahoo.com>
2026-04-27 14:24:58 -0400

    display high/low temps

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 - Create an app that visualizes a temperature blanket for a chosen span of time with a chosen amount of custom colors
 - Connect to a weather API
 
-## TO DO NEXT
+## NOTES TO MYSELF
 - Update form input to:
   - validate input type
-  - default to F and EST
-- Insert variables into url for API call
+- Issues:
+  - Zip code only works with strictly numeric zip codes
+    - This is a limitation of the geocoding API (ex: when you search for Toronto, it's postal code of 'M5V 3L9' does not appear in the json). I'll have to look into this more later as it's a minor issue that won't impact playtesting for a personal and private project. After looking into this, I've returned the input to numeric rather than alpha.
+    - After looking even more into this, I have discovered that the zip codes stored here are unreliable (ex: searching 79109, an Amarillo, TX zip code, returns a city in France. The Amarillo json does not include that zip code...) I will be re-doing this with city name and will:
+      - **RETURN** to add validation (list top 10 matches and have the user select the correct one)
 
 ### Acknowledgements & References
 - CSS Reset from [Josh Comeau](https://www.joshwcomeau.com/css/custom-css-reset/)
--- public/index.js
+++ public/index.js
@@ -1,49 +1,85 @@
 const form = document.getElementById('DataForm')
 
 let year
-let zipCode
+// let zipCode
+let city
 let tempScale
 let latitude
 let longitude
+let maxHighTemp
+let maxLowTemp
+let minHighTemp
+let minLowTemp
 
 // || COLLECT FORM INPUT AND UPDATE VARIABLES ||
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
     year = document.getElementById('year').value
-    zipCode = document.getElementById('zipCode').value
+    city = document.getElementById('city').value
+    // zipCode = document.getElementById('zipCode').value
     tempScale = document.getElementById('tempScale').value
-
-    console.log(year)
-    console.log(zipCode)
-    console.log(tempScale)
     
     // || DISPLAY SEARCH PARAMETERS ||
     const searchResults = document.getElementById('SearchResults')
     
+    const resultsHeader = document.createElement('h2')
     const searchYear = document.createElement('p')
-    const searchZipCode = document.createElement('p')
+    const searchCity = document.createElement('p')
+    // const searchZipCode = document.createElement('p')
     const searchTempScale = document.createElement('p')
+    const searchMaxHighTemp = document.createElement('p')
+    const searchMinHighTemp = document.createElement('p')
+    // const searchMaxLowTemp = document.createElement('p')
+    // const searchMinLowTemp = document.createElement('p')
 
-    searchYear.textContent = `Year: ${year}`
-    searchZipCode.textContent = `Zip Code: ${zipCode}`
-    searchTempScale.textContent = `Temperature Scale: ${tempScale}`
-
-    searchResults.appendChild(searchYear)
-    searchResults.appendChild(searchZipCode)
-    searchResults.appendChild(searchTempScale)
-
-    await getCoordinates(zipCode)
+    await getCoordinates(city)
     await loadWeatherData(year, latitude, longitude, tempScale)
+
+    resultsHeader.textContent = 'Displaying results for:'
+    searchYear.textContent = `Year: ${year}`
+    searchCity.textContent = `City: ${city}`
+    // searchZipCode.textContent = `Zip Code: ${zipCode}`
+    searchTempScale.textContent = `Temperature Scale: ${tempScale}`
+    searchMaxHighTemp.textContent = `The max high temp is: ${maxHighTemp}`
+    searchMinHighTemp.textContent = `The min high temp is: ${minHighTemp}`
+    // searchMaxLowTemp.textContent = `The max low temp is: ${maxLowTemp}`
+    // searchMinLowTemp.textContent = `The min low temp is: ${minLowTemp}`
+
+    searchResults.replaceChildren(
+        searchResults.appendChild(resultsHeader),
+        searchResults.appendChild(searchYear),
+        searchResults.appendChild(searchCity),
+        searchResults.appendChild(searchTempScale),
+        searchResults.appendChild(searchMaxHighTemp),
+        searchResults.appendChild(searchMinHighTemp)
+        // searchResults.appendChild(searchMaxLowTemp),
+        // searchResults.appendChild(searchMinLowTemp)
+        )
 })
 
-async function loadWeatherData (year, latitude, longitude, tempScale = "fahrenheit") {
+export async function loadWeatherData (year, latitude, longitude, tempScale = "fahrenheit") {
     try {
         const res = await fetch(`https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${year}-01-01&end_date=${year}-12-31&daily=temperature_2m_max,temperature_2m_min&temperature_unit=${tempScale}`, {
             method: "GET"
         })
         const data = await res.json()
-        console.log(data)
+        console.log(data.daily.temperature_2m_max)
+        
+        // Find max/min of HIGH temps for the year
+        let maxHighTemps = data.daily.temperature_2m_max
+        maxHighTemp = Math.max(...maxHighTemps)
+                
+        let minHighTemps = data.daily.temperature_2m_max
+        minHighTemp = Math.min(...minHighTemps)
+        
+        // Find max/min LOW temps for the year
+        // let maxLowTemps = data.daily.temperature_2m_min
+        // maxLowTemp = Math.max(...maxLowTemps)
+        
+        // let minLowTemps = data.daily.temperature_2m_min
+        // minLowTemp = Math.min(...minLowTemps)
+
     } catch (error) {
         console.error('error', error.message)
     }
@@ -51,9 +87,12 @@ async function loadWeatherData (year, latitude, longitude, tempScale = "fahrenhe
 
 // || CONVERT ZIP CODE TO COORDINATES ||
     
-async function getCoordinates (zipCode) {
+async function getCoordinates (city) {
     try {
-        const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${zipCode}&language=en&format=json`)
+        const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&language=en&format=json`)
+
+        // This link includes formatting to show the top 10 results
+        // https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json
 
         const data = await res.json()
         
@@ -61,5 +100,22 @@ async function getCoordinates (zipCode) {
         longitude = data.results[0].longitude
     } catch (e) {
     console.error(e)
+    }
 }
-}
+// async function getCoordinates (zipCode) {
+//     try {
+//         const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${zipCode}&language=en&format=json`)
+
+//         const data = await res.json()
+        
+//         latitude = data.results[0].latitude
+//         longitude = data.results[0].longitude
+//     } catch (e) {
+//     console.error(e)
+// }
+// }
+
+// || COLOR SELECTION ||
+
+const colorForm = document.getElementById('ColorForm')
+
